8c2cf0389a40f776a16adb14f643d5884f7c0ff2
Owen Connors <owenconnors1@gmail.com>
2023-12-19 23:34:05 +0000

    Add deploy workflow

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+export SCRIPT_DIR=$(dirname "$(realpath $0)")
+cd $SCRIPT_DIR && echo "Running in $SCRIPT_DIR"
+if [ -z $NO_PULL ]; then git pull; fi
+
+# Build main
+./zola build --output-dir ../public_html/recipes --force
